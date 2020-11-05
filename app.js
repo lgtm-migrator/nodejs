8cb3fff995c722012003b32ddc91f7f2df0fc2ae
Restyled.io <commits@restyled.io>
2020-11-05 21:21:56 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
-const uinput =
-    process.argv[2]
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+const uinput = process.argv[2];
 
-    // Goal: Use both destructuring and property shorthand in weather app
-    //
-    // 1. Use destructuring in app.js, forecast.js, and geocode.js
-    // 2. Use property shorthand in forecast.js and geocode.js
-    // 3. Test your work and ensure app still works
+// Goal: Use both destructuring and property shorthand in weather app
+//
+// 1. Use destructuring in app.js, forecast.js, and geocode.js
+// 2. Use property shorthand in forecast.js and geocode.js
+// 3. Test your work and ensure app still works
 
-    if (!uinput) {
-  return console.log('Please enter a location you would like the forecast for!')
-}
-else {
-  geocode(uinput, (error, {latitude, longitude, location} = {}) => {
+if (!uinput) {
+  return console.log(
+    "Please enter a location you would like the forecast for!"
+  );
+} else {
+  geocode(uinput, (error, { latitude, longitude, location } = {}) => {
     if (error) {
-      return console.log(error)
+      return console.log(error);
     }
 
     forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
-        return console.log(error)
+        return console.log(error);
       }
 
-      console.log(location)
-      console.log(forecastData)
-    })
-  })
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,27 +1,27 @@
-const request = require('postman-request')
+const request = require("postman-request");
 
-const geocode =
-    (address, callback) => {
-      const url =
-          'http://api.mapbox.com/geocoding/v5/mapbox.places/' +
-          encodeURIComponent(address) +
-          '.json?access_token=pk.eyJ1IjoiYnVmZmFsb2Jvb21lciIsImEiOiJja2Mwc2F6dzAwbWhxMnN1cmVyNDJuYW1iIn0.2nQSSXm5cawBNa6fVb7FIg'
+const geocode = (address, callback) => {
+  const url =
+    "http://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoiYnVmZmFsb2Jvb21lciIsImEiOiJja2Mwc2F6dzAwbWhxMnN1cmVyNDJuYW1iIn0.2nQSSXm5cawBNa6fVb7FIg";
 
-      request({url, json : true}, (error, {body}) => {
-        if (error) {
-          callback('Check your network connection!', undefined)
-        } else if (body.features.length === 0) {
-          callback(
-              'Unable to find that location! Please check the address you entered and try again...',
-              undefined)
-        } else {
-          callback(undefined, {
-            latitude : body.features[0].center[1],
-            longitude : body.features[0].center[0],
-            location : body.features[0].place_name
-          })
-        }
-      })
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Check your network connection!", undefined);
+    } else if (body.features.length === 0) {
+      callback(
+        "Unable to find that location! Please check the address you entered and try again...",
+        undefined
+      );
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
     }
+  });
+};
 
-                           module.exports = geocode
+module.exports = geocode;
