0fdacc484ec2fae3d9a5a48e5b4ecdf5c4143348
Hudson Woomer <hudson.t.woomer@gmail.com>
2020-11-05 21:20:29 +0000

    Committed change after destructuring objects and using shorthand for the data calls.

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,16 +1,22 @@
-
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=c39ceffe6be9b65a901364cbe6d84925&query='+latitude+','+longitude+'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=c39ceffe6be9b65a901364cbe6d84925&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({url: url, json: true}, (error, response) => {
+    request({
+        url,
+        json: true
+    }, (error, {
+        body
+    }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to connect to weather service!', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions.toString() + ', It is currently ' + response.body.current.temperature.toString() + ' degrees out. It feels like ' + response.body.current.feelslike.toString() + ' degrees out.')
+            callback(undefined,
+                body.current.weather_descriptions.toString() +
+                ', It is currently ' + body.current.temperature.toString() + ' degrees out. It feels like ' + body.current.feelslike.toString() + ' degrees out.')
         }
     })
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,19 +1,23 @@
-
 const request = require('postman-request')
 
 const geocode = (address, callback) => {
     const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYnVmZmFsb2Jvb21lciIsImEiOiJja2Mwc2F6dzAwbWhxMnN1cmVyNDJuYW1iIn0.2nQSSXm5cawBNa6fVb7FIg'
 
-    request({url: url, json: true}, (error, response) => {
+    request({
+        url,
+        json: true
+    }, (error, {
+        body
+    }) => {
         if (error) {
             callback('Check your network connection!', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find that location! Please check the address you entered and try again...', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
