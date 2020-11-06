37e9d77e0769e2993a1dfde280ef63411f132153
Hudson Woomer <hudson.t.woomer@gmail.com>
2020-11-06 16:41:53 +0000

    Changes with dynamic webpages added to include express server and hbs dynamic pages instead of static html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,55 @@
+const path = require('path')
 const express = require('express')
 
+//Creation/call to the express app
 const app = express()
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../Public')
+const viewsPath = path.join(__dirname, '../templates')
 
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//Call to the index page
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Hudson Woomer'
+    })
 })
 
+//Call to the about page 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Hudson Woomer'
+    })
+})
+
+//Call to the help page 
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.render('help', {
+        title: 'Help Page',
+        msg: 'This page is where we help dumb MFrs like you!'
+    })
 })
-
+ 
+ //Weather page not called 
+app.get('/weather', (req, res) => {
+    res.send({
+        location: Baltimore,
+        forecast: 'you are leaving early today!',
+    })
+})
+//----REQUIRED PAGES-----
 // app.com
 // app.com/help 
 // app.com/about
-
+// app.com/weather
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
